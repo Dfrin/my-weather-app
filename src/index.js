87c53f18928c645efb63b94b2082adc001d3d819
Dfrin <d.afrin@hotmail.com>
2023-05-08 07:27:08 +0200

    API update

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+  let apiKey = "0a75bf8091478575bafa303e89932e4b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
@@ -83,7 +83,7 @@ function weatherNow(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+  let apiKey = "0a75bf8091478575bafa303e89932e4b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(weatherNow);
 }
